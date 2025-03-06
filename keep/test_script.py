81794e57838503b1c612ed94645e7b0020901d99
Jkianboringot <jkianboringot@gmail.com>
2025-03-06 23:42:30 +0800

    added other file

---
+++ keep/test_script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def greet(name):
+    return name
+def add(a,b):
+    return a+b
+greet('jkin')
+import os
+
+print("Current Working Directory:", os.getcwd())
+
